823e3c4edf92e07051b9f0faaf7f808805757e56
Kathryn Kassapides <kt@Kathryns-MacBook-Pro.local>
2022-06-28 13:14:59 -0400

    pokemon-detail components and router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BookComponent } from './components/book/book.component';
 import { PokemonListComponent } from './components/pokemon-list/pokemon-list.component';
+import { DetailComponent } from './components/detail/pokemon-detail.component';
 
 const routes: Routes = [
   { path: '', component: BookComponent },
   { path: 'pokemon-list', component: PokemonListComponent },
+  { path: 'pokemon-detail', component: DetailComponent },
   { path: '**', redirectTo: '' }
 ];
 
